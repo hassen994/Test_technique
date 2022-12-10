10ce08b1b89b5f445376ad13b2386c6f2ae1386b
hassen994 <hassen.aloui@esprit.tn>
2022-12-10 22:30:56 +0100

    technical test done

---
+++ src/main/java/tn/innofab/embauche/test_technique/TestTechniqueApplication.java
@@ -0,0 +1,13 @@
+package tn.innofab.embauche.test_technique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechniqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTechniqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/innofab/embauche/test_technique/TestTechniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.innofab.embauche.test_technique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechniqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
